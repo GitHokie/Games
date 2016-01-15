c6af0fca7561cd9d49770841a4b6f1a5e5eb7a4d
Kurt Schron <kurt.schron@gmail.com>
2016-01-15 11:52:28 -0500

    Adding FizzBuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+#A program that prints the numbers 1 to 100. 
+#Multiples of 3 print 'Fizz'
+#Multiples of 5 pring 'Buzz'
+#Multiples of 3 & 5 print 'FizzBuzz'
+
+
+for i in range(1,101):
+	if i % 3 == 0 and i % 5 == 0:
+		print "FizzBuzz"
+	elif i % 3 == 0:
+		print "Fizz"
+	elif i % 5 == 0:
+		print "Buzz"
+	else:
+		print i
+
